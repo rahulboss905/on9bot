e73543f4efc368901731a04aced7cca5cea2df44
rahulboss905 <166490010+rahulboss905@users.noreply.github.com>
2025-08-13 02:00:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,37 +1,30 @@
-import os  # remove this import and change BOT_TOKEN below to your bot token
+import os
 from typing import Iterable
 
-from telegram import Bot, User, Chat
-
-# Create a bot by talking to BotFather (t.me/BotFather).
-# You are required to set a username and suggested to disable your bot's privacy mode.
-
-# Replace all of these constants
-BOT_TOKEN: str = os.environ["BOT_TOKEN"]  # Replace with bot token. Delete this env var thing and remove os import
+# Bot configuration
+BOT_TOKEN: str = os.environ["BOT_TOKEN"]
 OWNER_ID: int = 7456681709  # Your user id
 ADMIN_GROUP_ID: int = -1002897249216  # Admin group chat id
 SPECIAL_GROUP_ID: int = -1002897249216  # Special group chat id
 HEROKU_APP_NAME: str = "on9bot"  # Heroku app name
 
-#                              C A N  U S E  F O R E V E R
+# Users who can use /tag9 command
 CAN_USE_TAG9: Iterable[int] = (OWNER_ID, 190726372, 106665913, 537248339, 540933895, 401742123)
 
-# Do not modify these
-nubbot: Bot = Bot(BOT_TOKEN)
+# GitHub source code link
+GITHUB_SOURCE_CODE_LINK: str = "https://github.com/your-repo"
 
-BOT: User = nubbot.get_me()
-BOT_USERNAME: str = "@" + BOT.username
-BOT_LINK: str = f"https://t.me/{BOT.username}"
+# Markdown error text
+MARKDOWN_ERROR_TEXT: str = "Error formatting text: {}\nMake sure you're using valid Markdown syntax."
 
-OWNER: User = nubbot.get_chat_member(OWNER_ID, OWNER_ID).user
-OWNER_USERNAME: str = "@" + OWNER.username
-OWNER_LINK: str = f"https://t.me/{OWNER.username}"
-OWNER_MENTION: str = f"[{OWNER.full_name}]({OWNER_LINK})"
+# Bot username (will be set asynchronously in main)
+BOT_USERNAME: str = None
 
-ADMIN_GROUP: Chat = nubbot.get_chat(ADMIN_GROUP_ID)
-SPECIAL_GROUP: Chat = nubbot.get_chat(SPECIAL_GROUP_ID)
+# Owner information
+OWNER_USERNAME: str = "your_username"  # Replace with your Telegram username without '@'
+OWNER_MENTION: str = f"[Your Name](https://t.me/{OWNER_USERNAME})"  # Replace "Your Name" with your actual name
 
-# Running locally (YOU MUST MAKE SURE YOU ARE RUNNING Python 3.6+, I USE F-STRINGS):
+# Running locally:
 # You only need to copy the .py files (excluding trash.py) from the repo to a folder.
 # Run "pip install python-telegram-bot" if you haven't. Remember to update it.
 # Fill in the required information in config.py.
@@ -48,19 +41,11 @@ SPECIAL_GROUP: Chat = nubbot.get_chat(SPECIAL_GROUP_ID)
 # You have added the environmental variable and make sure to run the code in PyCharm as well.
 
 
-# Deploying to Heroku
+# Deploying to Render/Heroku
 # You will need to copy all the files (excluding trash.py, README.md and LICENSE.md) in the repo to a folder.
 # Fill in the required information in config.py.
-# Create an account on Heroku.
-# Download Heroku on https://devcenter.heroku.com/articles/getting-started-with-python#set-up so you can run it on cmd.
-# Cd to the folder containing the On9bot files (run "cd {directory name}" in cmd)
-# Run the following commands:
-
-# heroku create (run "heroku create appname" if you would like to specify a name, else a random one will be given)
-# git init
-# heroku git:remote -a {your heroku app name}
-# git add .
-# git commit -am "{commit message}"*
-# git push heroku master*
-
-# *From now on, you have to run these two commands when you push changes.
+# Create an account on Render/Heroku.
+# Download Heroku CLI if using Heroku: https://devcenter.heroku.com/articles/getting-started-with-python#set-up
+# For Render, create a new Web Service and connect your repository
+# Add all required environment variables in your deployment environment:
+#   BOT_TOKEN, OWNER_ID, ADMIN_GROUP_ID, SPECIAL_GROUP_ID, HEROKU_APP_NAME, etc.
